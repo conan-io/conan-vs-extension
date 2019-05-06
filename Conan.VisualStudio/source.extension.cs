85ded64ae26b04b0a69477a6df865bbf6c2ab999
Javier G. Sogo <jgsogo@gmail.com>
2019-05-06 10:50:10 +0200

    another icon to Conan

--- Conan.VisualStudio/source.extension.cs
+++ Conan.VisualStudio/source.extension.cs
@@ -7,12 +7,12 @@ namespace Conan.VisualStudio
 {
     static class Vsix
     {
-        public const string Id = "VSConanPackage.cab4ca1d-6f42-438f-a2eb-ab74c8069056";
+        public const string Id = "VSConanPackage.4d0379e2-2698-4e66-89de-6ead71165e9f";
         public const string Name = "Conan Visual Studio Extension";
         public const string Description = @"The Conan Visual Studio Extention automates the use of the Conan C/C++ package manager for retrieving dependencies within Visual Studio projects.";
         public const string Language = "en-US";
         public const string Version = "1.0.0";
-        public const string Author = "Bincrafters";
-        public const string Tags = "conan c c++ packaging dependency management bincrafters";
+        public const string Author = "Conan";
+        public const string Tags = "conan c c++ packaging dependency management";
     }
 }
