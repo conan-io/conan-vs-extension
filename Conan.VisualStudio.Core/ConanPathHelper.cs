614e3011c18c5bc029714d4047e49be1cb72da96
SSE4 <tomskside@gmail.com>
2019-05-20 22:27:27 +0700

    - avoid adding the same props twice
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- Conan.VisualStudio.Core/ConanPathHelper.cs
+++ Conan.VisualStudio.Core/ConanPathHelper.cs
@@ -71,5 +71,12 @@ namespace Conan.VisualStudio.Core
         {
             return GetNearestConanfilePath(path);
         });
+
+        public static string NormalizePath(string path)
+        {
+            return Path.GetFullPath(new Uri(path).LocalPath)
+                       .TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar)
+                       .ToUpperInvariant();
+        }
     }
 }
