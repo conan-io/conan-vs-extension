f18501dd66155dc5a2b83d9c3ab492065395a271
Javier G. Sogo <jgsogo@gmail.com>
2019-06-26 22:57:51 +0200

    add example using a custom configuration

--- Conan.VisualStudio.Examples/ConanConfigExample/ConanConfigExample/ConanConfigExample.cpp
+++ Conan.VisualStudio.Examples/ConanConfigExample/ConanConfigExample/ConanConfigExample.cpp
@@ -4,5 +4,10 @@
 
 int main()
 {
-    fmt::printf("Hello, %s!", "world");
+    fmt::printf("Using fmt version '%d'\n", FMT_VERSION);
+    #ifdef FMT_STRING_ALIAS
+        // The CustomDebug configuration is mapped to the profile 'custom_config.profile'
+        //  where we are setting the value of the option 'fmt:with_fmt_alias=True'
+        fmt::printf("Using FMT_STRING_ALIAS\n");
+    #endif
 }
---
+++ Conan.VisualStudio.Examples/ConanConfigExample/ConanConfigExample/custom_config.profile
@@ -0,0 +1,11 @@
+[settings]
+os="Windows"
+arch="x86_64"
+build_type="Debug"
+compiler=Visual Studio
+compiler.version="16"
+compiler.toolset="v142"
+compiler.runtime="MDd"
+
+[options]
+fmt:with_fmt_alias=True
