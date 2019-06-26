c73abd0baa26818a1903ee1662a6458aec121eaa
Javier G. Sogo <jgsogo@gmail.com>
2019-06-26 16:57:43 +0200

    build extension and download artifact

---
+++ vsix/download_artifact.ps1
@@ -0,0 +1,33 @@
+$apiUrl = 'https://ci.appveyor.com/api'
+$token = '<your-api-token>'
+$headers = @{
+  "Authorization" = "Bearer $token"
+  "Content-type" = "application/json"
+}
+$accountName = $env:APPVEYOR_ACCOUNT_NAME
+$projectSlug = $env:APPVEYOR_PROJECT_SLUG
+
+$downloadLocation = 'C:\projects'
+
+# get project with last build details
+$project = Invoke-RestMethod -Method Get -Uri "$apiUrl/projects/$accountName/$projectSlug" -Headers $headers
+
+# we assume here that build has a single job
+# get this job id
+$jobId = $project.build.jobs[0].jobId
+
+# get job artifacts (just to see what we've got)
+$artifacts = Invoke-RestMethod -Method Get -Uri "$apiUrl/buildjobs/$jobId/artifacts" -Headers $headers
+
+# here we just take the first artifact, but you could specify its file name
+# $artifactFileName = 'Conan.VisualStudio\bin\Release\Conan.VisualStudio.vsix'
+$artifactFileName = $artifacts[0].fileName
+
+# artifact will be downloaded as
+$localArtifactPath = "$downloadLocation\$artifactFileName"
+
+# download artifact
+# -OutFile - is local file name where artifact will be downloaded into
+# the Headers in this call should only contain the bearer token, and no Content-type, otherwise it will fail!
+Invoke-RestMethod -Method Get -Uri "$apiUrl/buildjobs/$jobId/artifacts/$artifactFileName" `
+-OutFile $localArtifactPath -Headers @{ "Authorization" = "Bearer $token" }
