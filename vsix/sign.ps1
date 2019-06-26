33eb0bfaa70a64097b7ddb675903484d7c1cd10c
Javier G. Sogo <jgsogo@gmail.com>
2019-06-26 13:07:21 +0200

    sign before deploy

---
+++ vsix/sign.ps1
@@ -0,0 +1,11 @@
+# Info: http://www.visualstudioextensibility.com/2018/01/29/code-signing-a-vsix-package-targeting-multiple-visual-studio-versions/
+# vsixsigntool.exe sign /f CodeSigningCertificate.pfx /sha1 "<sha1 bytes>" /p MyPassword /fd sha256 MyVSIXProject.vsix
+
+$cert = (Get-Item .\vsix\conan_vs_extension.pfx).FullName
+$vsix = (Get-Item .\Conan.VisualStudio\bin\Release\Conan.VisualStudio.vsix).FullName
+Write-Host "vsix: $vsix"
+Write-Host "cert: $cert"
+
+$vsixSignTool = Join-Path $visualStudioInstallation 'VSSDK\VisualStudioIntegration\Tools\Bin\VSIXSignTool.exe'
+. $vsixSignTool sign /f "$cert" /p $env:vsix_sign /fd sha256 "$vsix"
+
