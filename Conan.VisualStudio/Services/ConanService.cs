9c8118778fa3cbc57fc0a12a9209ad3d849beae0
Javier G. Sogo <jgsogo@gmail.com>
2019-06-28 17:07:10 +0200

    capture exception for iinvalid command

--- Conan.VisualStudio/Services/ConanService.cs
+++ Conan.VisualStudio/Services/ConanService.cs
@@ -141,8 +141,11 @@ namespace Conan.VisualStudio.Services
                     ConanBuildType build = _settingsService.GetConanBuild();
                     bool update = _settingsService.GetConanUpdate();
 
+<<<<<<< HEAD
                     ProcessStartInfo process = conan.Install(project, configuration, generator, build, update, _errorListService);
 
+=======
+>>>>>>> capture exception for iinvalid command
                     string message = $"[Conan.VisualStudio] Calling process '{process.FileName}' " +
                                      $"with arguments '{process.Arguments}'";
                     Logger.Log(message);
@@ -152,6 +155,7 @@ namespace Conan.VisualStudio.Services
                     {
                         using (Process exeProcess = Process.Start(process))
                         {
+<<<<<<< HEAD
                             var tokenSource = new CancellationTokenSource();
                             var token = tokenSource.Token;
 
@@ -166,6 +170,25 @@ namespace Conan.VisualStudio.Services
 
                             Task.WaitAll(outputReader, errorReader);
                             
+=======
+                            int exitCode = await exeProcess.WaitForExitAsync();
+
+                            string output = await exeProcess.StandardOutput.ReadToEndAsync();
+                            string error = await exeProcess.StandardError.ReadToEndAsync();
+
+                            if (output.Length > 0)
+                            {
+                                Logger.Log(output);
+                                await logStream.WriteLineAsync(output);
+                            }
+
+                            if (error.Length > 0)
+                            {
+                                Logger.Log(error);
+                                await logStream.WriteLineAsync(error);
+                            }
+
+>>>>>>> capture exception for iinvalid command
                             if (exitCode != 0)
                             {
                                 message = $"Conan has returned exit code '{exitCode}' " +
@@ -175,7 +198,11 @@ namespace Conan.VisualStudio.Services
                                 Logger.Log(message);
                                 await logStream.WriteLineAsync(message);
                                 _errorListService.WriteError(message, logFilePath);
+<<<<<<< HEAD
                                 return false;
+=======
+                                return;
+>>>>>>> capture exception for iinvalid command
                             }
                             else
                             {
@@ -187,6 +214,7 @@ namespace Conan.VisualStudio.Services
                             }
                         }
                     }
+<<<<<<< HEAD
                     catch(System.ComponentModel.Win32Exception e)
                     {
                         message = $"[Conan.VisualStudio] Unhandled error running '{process.FileName}'" +
@@ -195,6 +223,12 @@ namespace Conan.VisualStudio.Services
                         await logStream.WriteLineAsync(message);
                         _errorListService.WriteError(message);
                         return false;
+=======
+                    catch(System.ComponentModel.Win32Exception)
+                    {
+                        message = $"Error running '{process.FileName}'. Is it a valid path to conan.exe?";
+                        _errorListService.WriteError(message);
+>>>>>>> capture exception for iinvalid command
                     }
                 }
             }
