2ade7961e57217f6fd2848c3bd0a8c7c61a659e8
SSE4 <tomskside@gmail.com>
2019-07-05 09:31:09 -0700

    - some logging
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- vsix/install_vsix.ps1
+++ vsix/install_vsix.ps1
@@ -4,4 +4,7 @@ $vsixPath = "C:\projects\Conan.VisualStudio.vsix"
 $visualStudioInstallation = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VSSDK -property installationPath
 $vsixInstaller = Join-Path $visualStudioInstallation 'Common7\IDE\VSIXInstaller.exe'
 
+Write-Host "visualStudioInstallation: $visualStudioInstallation"
+Write-Host "vsixInstaller: $vsixInstaller"
+
 . $vsixInstaller /q /a $vsixPath
