9b7ecde71825c88a810def090b839a42d668e495
Javier G. Sogo <jgsogo@gmail.com>
2019-07-17 17:49:05 +0200

    need to print the help, otherwise hangs

--- vsix/check_installed.ps1
+++ vsix/check_installed.ps1
@@ -9,5 +9,5 @@ Write-Host "devenv: $devenv"
 Set-AppveyorBuildVariable "vcvars64" $vcvars64
 
 $ss1 = . "$vcvars64"
-$ss2 = . "$devenv" /ConanVisualStudioVersion
+$ss2 = . "$devenv" /ConanVisualStudioVersion /?
 Write-Host "output: $ss2"
