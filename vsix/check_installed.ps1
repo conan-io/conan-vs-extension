d92fa236b998b60e0baca59e78931eae44e6fa92
Javier G. Sogo <jgsogo@gmail.com>
2019-07-17 13:53:58 +0200

    use com

--- vsix/check_installed.ps1
+++ vsix/check_installed.ps1
@@ -1,13 +1,12 @@
 
 $visualStudioInstallation = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VSSDK -property installationPath
 $vcvars64 = Join-Path $visualStudioInstallation 'VC\Auxiliary\Build\vcvars64.bat'
-$devenv = Join-Path $visualStudioInstallation 'Common7\IDE\devenv.exe'
+$devenv = Join-Path $visualStudioInstallation 'Common7\IDE\devenv.com'
 
 Write-Host "vcvars64: $vcvars64"
 Write-Host "devenv: $devenv"
 
 Set-AppveyorBuildVariable "vcvars64" $vcvars64
 
-. "$vcvars64" 
-$ss = . devenv /ConanVisualStudioVersion /?
+$ss = . "$vcvars64" && "$devenv" /ConanVisualStudioVersion /?
 Write-Host "output: $ss"
