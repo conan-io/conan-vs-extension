a2a5a31684bbe18d3639b51a21167f438aa69da2
Javier G. Sogo <jgsogo@gmail.com>
2019-07-19 13:04:12 +0200

    check publish get the proper vars

--- vsix/publish.ps1
+++ vsix/publish.ps1
@@ -4,13 +4,11 @@
 
 $visualStudioInstallation = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VSSDK -property installationPath
 $vsixPublisher = Join-Path $visualStudioInstallation 'VSSDK\VisualStudioIntegration\Tools\Bin\VsixPublisher.exe'
-#Write-Host "vsixPublisher: $vsixPublisher"
 
-Write-Host "localArtifactPath: ${env:localArtifactPath}"
 $manifest = (Get-Item .\vsix\publish-manifest.json).FullName
-$vsix = (Get-Item .\Conan.VisualStudio\bin\Release\Conan.VisualStudio.vsix).FullName
-Write-Host "vsix: $vsix"
+# $vsix = (Get-Item .\Conan.VisualStudio\bin\Release\Conan.VisualStudio.vsix).FullName
+Write-Host "vsix: ${env:localArtifactPath}"
 Write-Host "manifest: $manifest"
 
 . $vsixPublisher login -publisherName conan-io -personalAccessToken $env:vsmarketplacetoken
-#. $vsixPublisher publish -payload "$vsix" -publishManifest "$manifest"
+#. $vsixPublisher publish -payload "${env:localArtifactPath}" -publishManifest "$manifest"
