cfa925326a596f541a4cbd75d80e341606fc329d
Javier G. Sogo <jgsogo@gmail.com>
2019-07-19 11:11:26 +0200

    use local artifact

--- vsix/install_vsix.ps1
+++ vsix/install_vsix.ps1
@@ -1,11 +1,10 @@
 
-$vsixPath = "C:\projects\Conan.VisualStudio.vsix"
-
 $visualStudioInstallation = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VSSDK -property installationPath
 $vsixInstaller = Join-Path $visualStudioInstallation 'Common7\IDE\VSIXInstaller.exe'
 
 Write-Host "visualStudioInstallation: $visualStudioInstallation"
 Write-Host "vsixInstaller: $vsixInstaller"
+Write-Host "localArtifactPath: ${env:localArtifactPath}"
 
-Start-Process -FilePath "$vsixInstaller" -ArgumentList "/q /a $vsixPath" -Wait -PassThru;
+Start-Process -FilePath "$vsixInstaller" -ArgumentList "/q /a ${env:localArtifactPath}" -Wait -PassThru;
 "OK" | Write-Host -ForegroundColor Green
