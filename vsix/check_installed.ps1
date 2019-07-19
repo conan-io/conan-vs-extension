ea6f3eabfbdbb24c5189d5dd7a244e4b036e9604
Javier G. Sogo <jgsogo@gmail.com>
2019-07-19 09:58:44 +0200

    call vcvars

--- vsix/check_installed.ps1
+++ vsix/check_installed.ps1
@@ -7,16 +7,16 @@ if (${env:APPVEYOR_BUILD_WORKER_IMAGE} -eq "Visual Studio 2015")
 else
 {
     $visualStudioInstallation = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VSSDK -property installationPath
-    # $vcvars64 = Join-Path $visualStudioInstallation 'VC\Auxiliary\Build\vcvars64.bat'
+    $vcvars64 = Join-Path $visualStudioInstallation 'VC\Auxiliary\Build\vcvars64.bat'
     $devenv = Join-Path $visualStudioInstallation 'Common7\IDE\devenv.com'
 }
 
-# Write-Host "vcvars64: $vcvars64"
+Write-Host "vcvars64: $vcvars64"
 Write-Host "devenv: $devenv"
 
 # Set-AppveyorBuildVariable "vcvars64" $vcvars64
 
-# . "$vcvars64"
+. "$vcvars64"
 $output = . "$devenv" /ConanVisualStudioVersion /?
 
 $pattern = "^${env:APPVEYOR_BUILD_VERSION}\s+Microsoft Visual Studio"  # Version + output from /? command
