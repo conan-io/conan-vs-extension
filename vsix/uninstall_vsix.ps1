42adf3721cc0cf6081ddff24c9c3381854b8968e
Javier G. Sogo <jgsogo@gmail.com>
2019-08-01 12:09:13 +0200

    show output

--- vsix/uninstall_vsix.ps1
+++ vsix/uninstall_vsix.ps1
@@ -5,4 +5,7 @@ $vsixInstaller = Join-Path $visualStudioInstallation 'Common7\IDE\VSIXInstaller.
 Write-Host "visualStudioInstallation: $visualStudioInstallation"
 Write-Host "vsixInstaller: $vsixInstaller"
 
-. $vsixInstaller /u:VSConanPackage.4d0379e2-2698-4e66-89de-6ead71165e9f
+$output = . $vsixInstaller /u:VSConanPackage.4d0379e2-2698-4e66-89de-6ead71165e9f
+Write-Host "Output was: $output"
+
+"OK" | Write-Host -ForegroundColor Green
