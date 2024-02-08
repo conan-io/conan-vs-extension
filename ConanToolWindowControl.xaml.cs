d4469e91ad182efaf75901f62fee09e0ae59ad17
czoido <mrgalleta@gmail.com>
2024-02-08 18:56:31 +0100

    wip

--- ConanToolWindowControl.xaml.cs
+++ ConanToolWindowControl.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.Windows;
 using System.Windows.Controls;
+using System.Collections.Generic;
 
 namespace conan_vs_extension
 {
@@ -15,13 +16,58 @@ namespace conan_vs_extension
         public ConanToolWindowControl()
         {
             this.InitializeComponent();
-            this.LoadHtmlContent();
+            LoadItemsIntoListView();
+            LoadHtmlContent();
         }
+
+        private void LoadItemsIntoListView()
+        {
+            var items = new List<string>
+            {
+                "open-dis-cpp", "open-simulation-interface", "open62541", "openal-soft", "openapi-generator", "openassetio", "openblas", "opencascade",
+                "opencl-clhpp-headers", "opencl-headers", "opencl-icd-loader", "opencolorio", "opencore-amr", "opencv", "openddl-parser",
+                "openebs", "openexr", "openfbx", "openfst", "openfx", "opengl-registry", "opengr", "opengv", "openh264", "openimageio",
+                "openjdk", "openjpeg"
+            };
+
+            foreach (var item in items)
+            {
+                PackagesListView.Items.Add(item);
+            }
+        }
+
         public void LoadHtmlContent()
         {
             string htmlContent = "<html><body><h1>Hello World!</h1>/body></html>";
             myWebBrowser.NavigateToString(htmlContent);
         }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (PackagesListView.SelectedItem is string selectedItem)
+            {
+                LoadHtmlContentForItem(selectedItem);
+            }
+        }
+
+        private void LoadHtmlContentForItem(string item)
+        {
+            string htmlTemplate = @"
+        <html>
+        <head>
+            <style>
+                body {{ font-family: 'Roboto', sans-serif; }}
+            </style>
+        </head>
+        <body>
+            <h1>{0}</h1>
+            <p>Hi! {0}.</p>
+        </body>
+        </html>";
+            string htmlContent = string.Format(htmlTemplate, item);
+            myWebBrowser.NavigateToString(htmlContent);
+        }
+
 
         /// <summary>
         /// Handles click on the button by displaying a message box.
