d734c9228c05de35b78c5d7bb00349c5f9b9748f
Daniel <danimanzaneque@gmail.com>
2024-02-15 12:06:33 +0200

    Feature: Generate profile files with compiler detection (#2)
    * Feature: Generate profile files with compiler detection
    
    * conan folder per project and map profile content
    
    * remove duplicate method
    
    ---------
    
    Co-authored-by: czoido <mrgalleta@gmail.com>

--- ConanToolWindowControl.xaml.cs
+++ ConanToolWindowControl.xaml.cs
@@ -9,10 +9,16 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using EnvDTE;
 using Microsoft.VisualStudio.Shell;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.VisualStudio.VCProjectEngine;
+using Microsoft.VisualStudio;
 using System.Collections;
+using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Reflection;
+using System.Runtime.InteropServices;
 
 
 namespace conan_vs_extension
@@ -299,8 +305,73 @@ target_link_libraries(your_target_name PRIVATE {cmakeTargetName})
 
         }
 
+        private string getProfileName(string vcConfigName)
+        {
+            return vcConfigName.Replace("|", "_");
+        }
+
+        private string getConanArch(string platform)
+        {
+            var archMap = new Dictionary<string, string>();
+            archMap["x64"] = "x86_64";
+            archMap["Win32"] = "x86";
+            return archMap[platform];
+        }
+
+        private string getConanCompilerVersion(string platformToolset)
+        {
+            var msvcVersionMap = new Dictionary<string, string>();
+            msvcVersionMap["v143"] = "193";
+            msvcVersionMap["v142"] = "192";
+            msvcVersionMap["v141"] = "191";
+            return msvcVersionMap[platformToolset];
+        }
+
         private void ShowPackages_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                DTE2 dte = (DTE2)ServiceProvider.GlobalProvider.GetService(typeof(DTE));
+                if (dte != null && dte.Solution != null && dte.Solution.Projects != null)
+                {
+                    foreach (Project project in dte.Solution.Projects)
+                    {
+                        if (project.Object is VCProject vcProject)
+                        {
+                            string projectDirectory = System.IO.Path.GetDirectoryName(project.FullName);
+                            string conanProjectDirectory = System.IO.Path.Combine(projectDirectory, ".conan");
+
+                            if (!Directory.Exists(conanProjectDirectory))
+                            {
+                                Directory.CreateDirectory(conanProjectDirectory);
+                            }
+
+                            foreach (VCConfiguration vcConfig in (IEnumerable)vcProject.Configurations)
+                            {
+                                string profileName = getProfileName(vcConfig.Name);
+                                string profilePath = System.IO.Path.Combine(conanProjectDirectory, profileName);
+
+                                if (!File.Exists(profilePath))
+                                {
+                                    string toolset = vcConfig.Evaluate("$(PlatformToolset)").ToString();
+                                    string compilerVersion = getConanCompilerVersion(toolset);
+                                    string arch = getConanArch(vcConfig.Evaluate("$(PlatformName)").ToString());
+                                    string buildType = vcConfig.ConfigurationName;
+                                    string profileContent = $"[settings]\narch={arch}\nbuild_type={buildType}\ncompiler=msvc\ncompiler.cppstd=14\ncompiler.runtime=dynamic\n" +
+                                        $"compiler.runtime_type={buildType}\ncompiler.version={compilerVersion}\nos=Windows";
+                                    File.WriteAllText(profilePath, profileContent);
+                                }
+                            }
+                        }
+                    }
+
+                    MessageBox.Show($"Generated profiles for actual project.", "Conan profiles generated", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"There was a problem generating the file: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private async Task UpdateJsonDataAsync()
